9b643e6832c31cce53df9ff385ea4a8b59612c7b
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-17 00:32:39 +0300

    fixed bugs

--- __logs.py
+++ __logs.py
@@ -1,27 +1,16 @@
+__help__ = "Устанавливает логирование"
 from rich import print
 import os
-from logging import FileHandler
-import logging
-from flask import Flask
 
-class Logger:
-    def __init__(self, app):
-        self.app: Flask = app.flask
-        if not os.path.exists("logs"):
-            os.mkdir("logs")
-        handler = FileHandler("logs/app.log")
-        handler.setLevel(logging.INFO)
-        # Формат логов: время, IP-адрес клиента, метод HTTP, URL
-        formatter = logging.Formatter('%(asctime)s - %(remote_addr)s - %(method)s - %(url)s')
-        handler.setFormatter(formatter)
-        self.app.logger.addHandler(handler)
+base = os.sep.join(__file__.split(os.sep)[:-1])
 
-    def warning(msg):
-        print(f"[yellow][!] {msg}[/yellow]")
-    
-    def success(msg):
-        print(f"[green][+] {msg}[/green]")
+def run(*args, **kwargs):
+	arg = kwargs["args"]
+	if "init" in arg:
+		if os.path.exists("AEngineApps"):
+			if not os.path.exists("AEngineApps/logging.py"):
+				with open(os.path.join(base, "__logs.py"), encoding="utf-8") as file, \
+				open(os.path.join("AEngineApps/logging.py"),"w", encoding="utf-8") as file_to:
+					file_to.write(file.read())
 
-    def fail(msg):
-        print(f"[red][-] {msg}[/red]")
 
