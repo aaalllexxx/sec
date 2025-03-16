6fcb7ca2ba790c3772c2f209126183cfc56fc3ce
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-17 00:37:04 +0300

    fixed bugs

--- __logs.py
+++ __logs.py
@@ -10,8 +10,7 @@ def run(*args, **kwargs):
 		if os.path.exists("AEngineApps"):
 			if not os.path.exists("AEngineApps/logging.py"):
 				open(os.path.join("AEngineApps/logging.py"),"w", encoding="utf-8").close()
-            with open(os.path.join(base, "__logs.py"), encoding="utf-8") as file, \
-            open(os.path.join("AEngineApps/logging.py"),"w", encoding="utf-8") as file_to:
-                file_to.write(file.read())
+			with open(os.path.join(base, "__logs.py"), encoding="utf-8") as file, open(os.path.join("AEngineApps/logging.py"),"w", encoding="utf-8") as file_to:
+				file_to.write(file.read())
 
 
